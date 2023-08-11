29d684d5e8f6572be4d0bde3e2e63c152f0359ea
Nishtha Arora <139135471+NishthaArora1910@users.noreply.github.com>
2023-08-11 22:48:02 +0530

    Create leftTriangle.java

---
+++ leftTriangle.java
@@ -0,0 +1,15 @@
+public class leftTrianlge {
+  public static void main(String[] args) {
+
+    // size of the triangle
+    int size = 5;
+    // loop to print the pattern
+    for (int i = 0; i < size; i++) {
+      // print column
+      for (int j = 0; j <= i; j++) {
+        System.out.print("*");
+      }
+      System.out.println();
+    }
+  }
+}
